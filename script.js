d44a993a9406b49547420043435af47f96a8ae0a
gdickey273 <gdickey273@gmail.com>
2020-07-23 16:20:50 -0400

    if once both selectedDate and partyNumber are set, checkAvailability() is run each time either changes

--- script.js
+++ script.js
@@ -1,3 +1,64 @@
+
+var indoorTables = [
+  {name: "Table 1",
+  capacity: 2,
+  reservations: []
+  },
+  {name: "Table 2",
+  capacity: 2,
+  reservations: []
+  },
+  {name: "Table 3",
+  capacity: 4,
+  reservations: []
+  },
+  {name: "Table 4",
+  capacity: 4,
+  reservations: []
+  },
+  {name: "Table 5",
+  capacity: 6,
+  reservations: []
+  },
+  {name: "Table 6",
+  capacity: 4,
+  reservations: []
+  },
+  {name: "Table 7",
+  capacity: 4,
+  reservations: []
+  },
+  {name: "Table 8",
+  capacity: 6,
+  reservations: []
+  }
+]
+var outdoorTables = [
+  {name: "Table 100",
+  capacity: 5,
+  reservations: []
+  },
+  {name: "Table 102",
+  capacity: 5,
+  reservations: []
+  },
+  {name: "Table 105",
+  capacity: 5,
+  reservations: []
+  },
+  {name: "Table 106",
+  capacity: 6,
+  reservations: []
+  }
+]
+var partyNumber = 0;
+var selectedDate = undefined;
+var dayOfWeek;
+var reservationStartTime;
+var reservationEndTime;
+
+
+//Something to do with the calendar widget
 $(document).ready(function(){
   var date_input=$('input[name="date"]'); //our date input has the name "date"
   var container=$('.bootstrap-iso form').length>0 ? $('.bootstrap-iso form').parent() : "body";
@@ -9,11 +70,32 @@ $(document).ready(function(){
   })
 })
 
+//Saves party number to variable when party number button is clicked
 $(".partyNumberButton").on("click", function(){
   $(".partyNumberButton").each(function(){
     $(this).removeClass("active");
   })
   $(this).addClass("active");
   var partyButton = $(this).children();
-  console.log(parseInt(partyButton.attr("id").split("-")[1]));
+  partyNumber = parseInt(partyButton.attr("id").split("-")[1]);
+
+  //If date is already chosen, run checkAvailability() once party number is set
+  if(selectedDate != undefined){
+    checkAvailability();
+  }
 });
+
+//Saves date to selectedDate var and day of week (0-6) to dayOfWeek var
+$("#date").change(function(){
+  selectedDate = moment($(this).val(), "L");
+  dayOfWeek = selectedDate.day();
+
+  //If party number is already set, run checkAvailability() once day is chosen
+  if (partyNumber > 0){
+    checkAvailability();
+  }
+});
+
+function checkAvailability(){
+  
+}
