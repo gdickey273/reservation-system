ef950639bed9ce4fd2e63558cbc45e7cc326b0fd
gdickey273 <gdickey273@gmail.com>
2020-08-03 08:32:15 -0400

    working on initializeTables function but first need to fix multiple 'change' triggers on the calendar widget

--- script.js
+++ script.js
@@ -1,131 +1,157 @@
 
 
 
-  // Get a reference to the database service
+// Get a reference to the database service
 
-  var indoorTables = [
-    {
-      tableNumber: "1",
-      capacity: 2,
-      reservations: []
-    },
-    {
-      tableNumber: "2",
-      capacity: 2,
-      reservations: []
-    },
-    {
-      tableNumber: "3",
-      capacity: 4,
-      reservations: []
-    },
-    {
-      tableNumber: "4",
-      capacity: 4,
-      reservations: []
-    },
-    {
-      tableNumber: "5",
-      capacity: 6,
-      reservations: []
-    },
-    {
-      tableNumber: "6",
-      capacity: 4,
-      reservations: []
-    },
-    {
-      tableNumber: "7",
-      capacity: 4,
-      reservations: []
-    },
-    {
-      tableNumber: "8",
-      capacity: 6,
-      reservations: []
-    }
-  ]
-  var outdoorTables = [
-    {
-      tableNumber: "100",
-      capacity: 5,
-      reservations: []
-    },
-    {
-      tableNumber: "102",
-      capacity: 5,
-      reservations: []
-    },
-    {
-      tableNumber: "105",
-      capacity: 5,
-      reservations: []
-    },
-    {
-      tableNumber: "106",
-      capacity: 6,
-      reservations: []
-    }
-  ]
-  var partyNumber = 0;
-  var selectedDate = undefined;
-  var dayOfWeek;
-  var time;
-  var earliestResTime;
-  var latestResTime;
+var indoorTables = [
+  {
+    tableNumber: "1",
+    capacity: 2,
+    reservations: []
+  },
+  {
+    tableNumber: "2",
+    capacity: 2,
+    reservations: []
+  },
+  {
+    tableNumber: "3",
+    capacity: 4,
+    reservations: []
+  },
+  {
+    tableNumber: "4",
+    capacity: 4,
+    reservations: []
+  },
+  {
+    tableNumber: "5",
+    capacity: 6,
+    reservations: []
+  },
+  {
+    tableNumber: "6",
+    capacity: 4,
+    reservations: []
+  },
+  {
+    tableNumber: "7",
+    capacity: 4,
+    reservations: []
+  },
+  {
+    tableNumber: "8",
+    capacity: 6,
+    reservations: []
+  }
+]
+var outdoorTables = [
+  {
+    tableNumber: "100",
+    capacity: 5,
+    reservations: []
+  },
+  {
+    tableNumber: "102",
+    capacity: 5,
+    reservations: []
+  },
+  {
+    tableNumber: "105",
+    capacity: 5,
+    reservations: []
+  },
+  {
+    tableNumber: "106",
+    capacity: 6,
+    reservations: []
+  }
+]
+var partyNumber = 0;
+var selectedDate = undefined;
+var dayOfWeek;
+var time;
+var earliestResTime;
+var latestResTime;
+var dataObj;
 
-  var cloud = firebase.firestore();
+var cloud = firebase.firestore();
 
 
-  //Something to do with the calendar widget
-  $(document).ready(function () {
-    var date_input = $('input[name="date"]'); //our date input has the name "date"
-    var container = $('.bootstrap-iso form').length > 0 ? $('.bootstrap-iso form').parent() : "body";
-    date_input.datepicker({
-      format: 'mm/dd/yyyy',
-      container: container,
-      todayHighlight: true,
-      autoclose: true,
-    })
+
+//Something to do with the calendar widget
+$(document).ready(function () {
+  var date_input = $('input[name="date"]'); //our date input has the name "date"
+  var container = $('.bootstrap-iso form').length > 0 ? $('.bootstrap-iso form').parent() : "body";
+  date_input.datepicker({
+    format: 'mm/dd/yyyy',
+    container: container,
+    todayHighlight: true,
+    autoclose: true,
   })
+})
 
-  //Saves party number to variable when party number button is clicked
-  $(".partyNumberButton").on("click", function () {
-    $(".partyNumberButton").each(function () {
-      $(this).removeClass("active");
-    })
-    $(this).addClass("active");
-    var partyButton = $(this).children();
-    partyNumber = parseInt(partyButton.attr("id").split("-")[1]);
+//Saves party number to variable when party number button is clicked
+$(".partyNumberButton").on("click", function () {
+  $(".partyNumberButton").each(function () {
+    $(this).removeClass("active");
+  })
+  $(this).addClass("active");
+  var partyButton = $(this).children();
+  partyNumber = parseInt(partyButton.attr("id").split("-")[1]);
 
-    //If date is already chosen, run checkAvailability() once party number is set
-    if (selectedDate != undefined) {
-      checkAvailability();
-    }
-  });
+  //If date is already chosen, run checkAvailability() once party number is set
+  if (selectedDate != undefined) {
+    checkAvailability();
+  }
+});
 
-  //Saves date to selectedDate var and day of week (0-6) to dayOfWeek var
-  $("#date").change(function () {
-    console.log($(this).val());
-    selectedDate = moment($(this).val(), "L");
-    dayOfWeek = selectedDate.day();
+//Saves date to selectedDate var and day of week (0-6) to dayOfWeek var
+$("#date").change(function () {
+  if (selectedDate !== moment($(this).val().replace(/\//g, ""), "L")) {
+    
+  console.log($(this).val());
+  selectedDate = moment($(this).val().replace(/\//g, ""), "L");
+  dayOfWeek = selectedDate.day();
+  console.log(selectedDate);
 
-    //If party number is already set, run checkAvailability() once day is chosen
-    if (partyNumber > 0) {
-      checkAvailability();
-    }
-  });
+  initializeTables();
 
-  function initializeTables(){
-
+  //If party number is already set, run checkAvailability() once day is chosen
+  if (partyNumber > 0) {
+    checkAvailability();
+  }
   }
 
-  function checkAvailability() {
+  return false;
+
+});
+
+function initializeTables() {
+  var docPath = "scheduleByDate/" + selectedDate._i + "/";
+  indoorTables.forEach(function (table) {
+    cloud.doc(docPath + "insideTables/" + table.tableNumber).get().then(function (doc) {
+      var data = doc.data();
+      console.log(data);
+
+      if (data != undefined && data.reservations != undefined) {
+        console.log(data.reservations);
+
+        data.reservations.forEach(function (resObj) {
+          table.reservations.push(resObj);
+        })
+      }
+
+    });
+  });
+}
+
+function checkAvailability() {
 
 
-  }
+}
 
-  
+
 
 
 
@@ -133,7 +159,7 @@
   //Push element to a nested firestore array
 
   // cloud.doc("scheduleByDate/08102020/insideTables/1").update({
-    
+
   //   reservations: 
   //   firebase.firestore.FieldValue.arrayUnion(
   //     {time: "20:00",
